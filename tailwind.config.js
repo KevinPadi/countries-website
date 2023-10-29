3e94452371d6a1c719386e2548728084187b4230
Kevin <kevpadilla01@gmail.com>
2023-10-29 19:35:30 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        darkBlue: 'hsl(209, 23%, 22%)',
+        veryDarkBlue: 'hsl(200, 15%, 8%)',
+        darkGray: 'hsl(0, 0%, 52%)',
+        lightGray: 'hsl(0, 0%, 98%)'
+      }
+    }
+  },
+  plugins: []
+}
