ddeb40e02169d3e3b7d7c528a4e00144537506d7
Kevin <kevpadilla01@gmail.com>
2023-11-01 01:33:13 -0300

    fix: responsive height

--- src/components/CountryFilterSection.jsx
+++ src/components/CountryFilterSection.jsx
@@ -4,7 +4,7 @@ import RegionFilter from './RegionFilter'
 
 function CountryFilterSection () {
   return (
-    <section className='w-full sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] h-40 md:h-20 px-5 sm:px-0 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
+    <section className='w-full sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] h-40 sm:h-20 px-5 sm:px-0 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <SearchBar />
       <RegionFilter />
     </section>
