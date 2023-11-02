6792930c21121f1f4f4052ed6b15aafb98f455a9
Kevin <kevpadilla01@gmail.com>
2023-11-02 01:07:49 -0300

    fix: padding bottom

--- src/components/CardsSections.jsx
+++ src/components/CardsSections.jsx
@@ -4,7 +4,7 @@ import CountryFilterSection from './CountryFilterSection'
 
 function CardsSections () {
   return (
-    <section className='w-full flex flex-col items-center gap-10 mt-28 md:mt-28 bg-lightGray dark:bg-[#202c37]'>
+    <section className='w-full flex flex-col items-center gap-10 mt-28 md:mt-28 bg-lightGray dark:bg-[#202c37] pb-10 lg:pb-20'>
       <CountryFilterSection />
       <section className='w-full sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] flex sm:flex-col justify-center sm:justify-between px-5 sm:px-0'>
         <div className='grid grid-cols-[repeat(auto-fit,minmax(235px,1fr))] gap-x-20 gap-y-10'>
