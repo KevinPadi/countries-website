deca29544122db69760f7d1ea2ae3cb89fb2f9fc
Kevin <kevpadilla01@gmail.com>
2023-11-13 20:57:16 -0300

    feat(CountryCard): implement country details card component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
-function CountryCard () {
+function CountryCard ({ name, population, region, capital, flag }) {
+  const populationFormated = population.toLocaleString('en-US')
+
   return (
     <article className='w-[235px] h-[350px] bg-white dark:bg-darkBlue rounded-md overflow-hidden drop-shadow-lg'>
-      <img className='w-full h-40' src='https://flagcdn.com/w320/br.png' alt='uruguay flag' />
+      <img className='w-full h-40' src={flag} alt={`${name} flag`} />
       <section className='flex flex-col gap-5 p-7'>
-        <h2 className='font-extrabold text-lg text-veryDarkBlue dark:text-white'>Brazil</h2>
+        <h2 className='font-extrabold text-lg text-veryDarkBlue dark:text-white'>{name}</h2>
         <section className='flex flex-col gap-1'>
           <div className='flex gap-1'>
-            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Population:</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>113.373.455</p>
+            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Population:</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>{populationFormated}</p>
           </div>
           <div className='flex gap-1'>
-            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Region:</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>America</p>
+            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Region:</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>{region}</p>
           </div>
           <div className='flex gap-1'>
-            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Capital</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>Brasilia</p>
+            <p className='font-semibold text-sm text-veryDarkBlue dark:text-white'>Capital:</p><p className='font-thin text-sm text-veryDarkBlue dark:text-white'>{capital}</p>
           </div>
         </section>
       </section>
