52548700f26ddb5eea248cf94698db2f2ac08d3a
Kevin <kevpadilla01@gmail.com>
2023-11-18 19:46:32 -0300

    feat (CountryCard): hover, borders and shadow added

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -4,9 +4,9 @@ function CountryCard ({ name, population, region, capital, flag }) {
   const populationFormated = population.toLocaleString('en-US')
 
   return (
-    <article className='w-[235px] h-[350px] bg-white dark:bg-darkBlue rounded-md overflow-hidden drop-shadow-lg'>
-      <img className='w-full h-40' src={flag} alt={`${name} flag`} />
-      <section className='flex flex-col gap-5 p-7'>
+    <article className='w-[235px] h-[350px] bg-white dark:bg-darkBlue rounded-md overflow-hidden shadow-lg group hover:shadow-2xl transition-shadow duration-250 border-1 border-gray-200 dark:border-0'>
+      <img className='w-full h-40 max-h-40 group-hover:scale-110 transition-transform' src={flag} alt={`${name} flag`} />
+      <section className='flex flex-col gap-5 p-7 border-t-1 border-gray-200 dark:border-t-0'>
         <h2 className='font-extrabold text-lg text-veryDarkBlue dark:text-white'>{name}</h2>
         <section className='flex flex-col gap-1'>
           <div className='flex gap-1'>
