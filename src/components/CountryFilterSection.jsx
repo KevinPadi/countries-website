8b7d743ef7ed419e788ddb5c5165a1ca4896eeec
Kevin <kevpadilla01@gmail.com>
2023-11-18 20:09:31 -0300

    feat(CountryFilterSection): pass state as props for dynamic filtering

--- src/components/CountryFilterSection.jsx
+++ src/components/CountryFilterSection.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import RegionFilter from './RegionFilter'
 
-function CountryFilterSection () {
+function CountryFilterSection ({ searchCountry, setSearchCountry }) {
   return (
     <section className='w-full sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] h-40 sm:h-20 px-5 sm:px-0 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
-      <SearchBar />
+      <SearchBar searchCountry={searchCountry} setSearchCountry={setSearchCountry} />
       <RegionFilter />
     </section>
   )
