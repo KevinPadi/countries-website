7b86239d1dfde940b688ff10ec91d90f53c54c28
Kevin <kevpadilla01@gmail.com>
2023-12-12 20:08:46 -0300

    fix: fetching logic added for country info

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,12 +1,24 @@
-import { React } from 'react'
+import { useState, useEffect } from 'react'
 import DetailsSectionHeader from './DetailsSectionHeader'
 import CountryDetails from './CountryDetails'
 import { useParams } from 'wouter'
 
-function DetailsPage ({ props, countriesData }) {
+function DetailsPage () {
+  const [countryData, setCountryData] = useState(null)
   const params = useParams()
   const name = params.name.replaceAll('%20', ' ')
-  const data = countriesData.filter((country) => country.name.common === name)
+
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,flags,tld,currencies,capital,region,subregion,languages,borders,population`)
+      .then(response => response.json())
+      .then(data => {
+        setCountryData(data)
+        console.log(data[0].flags)
+      })
+      .catch(error => {
+        console.error('Hubo un error al obtener los datos:', error)
+      })
+  }, [])
 
   return (
     <section className='w-full max-w-[400px] sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] mt-28 md:mt-28 pb-10 md:pb-20'>
@@ -14,9 +26,9 @@ function DetailsPage ({ props, countriesData }) {
         <DetailsSectionHeader />
         <div className='flex flex-col items-center lg:flex-row gap-10 lg:gap-20 '>
           <div className='w-[300px] lg:w-[850px]'>
-            <img className='w-full h-auto' src={data[0].flags.png} alt='flag' />
+            {countryData ? <img className='w-full h-auto' src={countryData[0].flags.png} alt='flag' /> : 'no cargó'}
           </div>
-          <CountryDetails data={data[0]} />
+          {countryData ? <CountryDetails data={countryData[0]} /> : 'no cargó'}
         </div>
       </div>
     </section>
