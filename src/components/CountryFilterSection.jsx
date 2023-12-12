8bb12a1710ea2bc08ea4a454908fbad3f3e27217
Kevin <kevpadilla01@gmail.com>
2023-12-12 20:13:21 -0300

    feat (CountryFilterSection): passing the filter logic to the RegionFilter

--- src/components/CountryFilterSection.jsx
+++ src/components/CountryFilterSection.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import SearchBar from './SearchBar'
 import RegionFilter from './RegionFilter'
 
-function CountryFilterSection ({ searchCountry, setSearchCountry }) {
+function CountryFilterSection ({ searchCountry, setSearchCountry, onSelect }) {
   return (
     <section className='w-full sm:max-w-[574px] lg:max-w-[865px] xl:max-w-[1185px] h-40 sm:h-20 px-5 sm:px-0 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <SearchBar searchCountry={searchCountry} setSearchCountry={setSearchCountry} />
-      <RegionFilter />
+      <RegionFilter onSelect={onSelect} />
     </section>
   )
 }
