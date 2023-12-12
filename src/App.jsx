1456ce6ae6ec20f940dac8e8351d6c0937eb5f5a
Kevin <kevpadilla01@gmail.com>
2023-12-12 20:06:51 -0300

    fix: the fetch logic is moved to CardsSections component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import CountryFilterSection from './components/CountryFilterSection'
+import CardsSections from './components/CardsSections'
 import Header from './components/Header'
+import DetailsPage from './components/DetailsPage'
+import { Route } from 'wouter'
 
 function App () {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex items-center flex-col min-h-screen' style={{ scrollbarGutter: 'stable' }}>
       <Header />
-      <main className='w-full mt-28 sm:mt-16 md:mt-28 bg-lightGray dark:bg-[#202c37]'>
-        <CountryFilterSection />
-      </main>
+      <Route path='/' component={() => <CardsSections />} />
+      <Route path='/country/:name' component={() => <DetailsPage />} />
     </div>
   )
 }
-
+//
 export default App
