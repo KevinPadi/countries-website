cf696dd63850d8c2d0a3e45683d03d48fcd71c33
Kevin <kevpadilla01@gmail.com>
2023-12-15 23:49:19 -0300

    feat(CountryCard): hover scale added

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -4,9 +4,9 @@ function CountryCard ({ name, population, region, capital, flag }) {
   const populationFormated = population.toLocaleString('en-US')
 
   return (
-    <article className='w-[235px] h-[350px] bg-white dark:bg-darkBlue rounded-md overflow-hidden shadow-lg group hover:shadow-2xl transition-shadow duration-250 border-1 border-gray-200 dark:border-0'>
+    <article className='w-[235px] h-[350px] bg-white dark:bg-darkBlue rounded-md overflow-hidden shadow-lg border-1 border-gray-200 dark:border-0 hover:scale-105 transition-transform'>
       <div className='w-full h-40 max-h-40 overflow-hidden'>
-        <img className='w-full h-full group-hover:scale-110 transition-transform' src={flag} alt={`${name} flag`} />
+        <img className='w-full h-full' src={flag} alt={`${name} flag`} />
       </div>
       <section className='flex flex-col gap-5 p-7 border-t-1 border-gray-200 dark:border-t-0'>
         <h2 className='font-extrabold text-lg text-veryDarkBlue dark:text-white'>{name}</h2>
