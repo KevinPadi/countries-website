65bbae561b0f51e9c645b99bc3883aed069b15a0
Kevin <kevpadilla01@gmail.com>
2023-12-16 03:06:28 -0300

    fix (App): Switch component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import CardsSections from './components/CardsSections'
 import Header from './components/Header'
 import DetailsPage from './components/DetailsPage'
-import { Route } from 'wouter'
+import { Route, Switch } from 'wouter'
 
 function App () {
   return (
     <div className='flex items-center flex-col min-h-screen' style={{ scrollbarGutter: 'stable' }}>
       <Header />
-      <Route path='/' component={() => <CardsSections />} />
-      <Route path='/country/:name' component={() => <DetailsPage />} />
+      <Switch>
+        <Route path='/' component={() => <CardsSections />} />
+        <Route path='/country/:name' component={() => <DetailsPage />} />
+      </Switch>
     </div>
   )
 }
-//
+
 export default App
