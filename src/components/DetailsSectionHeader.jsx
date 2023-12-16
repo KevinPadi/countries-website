29ca3c6303963a59b5f21c6e6098190929625b7c
Kevin <kevpadilla01@gmail.com>
2023-12-15 23:44:26 -0300

    feat (DetailsSectionHeader): hover scale added

--- src/components/DetailsSectionHeader.jsx
+++ src/components/DetailsSectionHeader.jsx
@@ -5,7 +5,7 @@ function DetailsSectionHeader () {
   return (
     <div className='h-10 md:h-24 flex justify-start items-center'>
       <Link href='/'>
-        <button className='flex items-center gap-2 px-10 h-10 rounded-sm text-xs drop-shadow-lg bg-white dark:bg-darkBlue'>
+        <button className='flex items-center gap-2 px-10 h-10 rounded-sm text-xs drop-shadow-lg bg-white dark:bg-darkBlue hover:scale-105 transition-transform'>
           <svg className='fill-darkBlue dark:fill-white' xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'><path d='M372.308-267.692 160-480l212.308-212.308L400.615-664 236.616-500H800v40H236.616l163.999 164-28.307 28.308Z' /></svg>
           Back
         </button>
